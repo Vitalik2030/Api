771ee3dbebea1c8bdcb9f0e3337d0723c617b3fc
Костенко Виталий <diable2011@mail.ru>
2022-05-08 11:38:00 +0300

    init

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+public class Main {public static void main(String[] args) {
+    Post post = new Post();
+    post.name = "Иван";
+    post.surname = "Иванов";
+    post.patronymic = "Иванович";
+    post.passport = "4444 № 444444";
+    post.phone = "+7 (999)-999-99-99";
+    post.subscription = true;
+    post.birthday = new FromDate();
+    post.birthday.day = 13;
+    post.birthday.month = 6;
+    post.birthday.year = 1999;
+}
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class Post { public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+    public FromDate birthday;
+}
+
